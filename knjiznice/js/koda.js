6a0b25d4b748c9aa28083c500f76f9e737cb9d93
Luka Tavčer <lukatavcer@yahoo.com>
2016-06-01 13:15:03 +0000

    Generiraj podatke protitip

--- knjiznice/js/koda.js
+++ knjiznice/js/koda.js
@@ -31,7 +31,190 @@ function getSessionId() {
  * @return ehrId generiranega pacienta
  */
 function generirajPodatke(stPacienta) {
-  ehrId = "";
+    ehrId = "";
+
+  switch(stPacienta) {
+    case 0:
+        $.ajax({
+        url: baseUrl + "/ehr",
+        type: 'POST',
+        success: function (data) {
+            var ehrId = data.ehrId;
+            var partyData = {
+                firstNames: "Luka",
+                lastNames: "Tavčer",
+                dateOfBirth: "1996-10-30T14:58",
+                partyAdditionalInfo: [{key: "ehrId", value: ehrId}]
+            };
+            $.ajax({
+                url: baseUrl + "/demographics/party",
+                type: 'POST',
+                contentType: 'application/json',
+                data: JSON.stringify(partyData),
+                success: function (party) {
+                    if (party.action == 'CREATE') {
+                        $("#kreirajSporocilo").html("<span class='label label-primary'>EHR 1 '" +
+                      ehrId + "'</span>");
+                    }
+                },
+                error: function(err) {
+                	$("#kreirajSporocilo").html("<span class='obvestilo label " +
+                "label-danger fade-in'>Napaka '" +
+                JSON.parse(err.responseText).userMessage + "'!");
+                }
+            });
+            
+        	var podatki = {
+        	    "ctx/language": "en",
+        	    "ctx/territory": "SI",
+        	    "ctx/time": "1938-10-30T14:58",
+        	    "vital_signs/height_length/any_event/body_height_length": "185",
+        	    "vital_signs/body_weight/any_event/body_weight": "55.0",
+        	   	"vital_signs/body_temperature/any_event/temperature|magnitude": "36.50",
+        	    "vital_signs/body_temperature/any_event/temperature|unit": "°C",
+        	    "vital_signs/blood_pressure/any_event/systolic": "118",
+        	    "vital_signs/blood_pressure/any_event/diastolic": "92",
+        	    "vital_signs/indirect_oximetry:0/spo2|numerator": "98"
+            };
+        	var parametriZahteve = {
+        	    ehrId: ehrId,
+        	    templateId: 'Vital Signs',
+        	    format: 'FLAT',
+        	    committer: 'Medicinska sestra Anja'
+        	};
+            
+            $.ajax({
+        	    url: baseUrl + "/composition?" + $.param(parametriZahteve),
+        	    type: 'POST',
+        	    contentType: 'application/json',
+        	    data: JSON.stringify(podatki)   
+            });
+        }
+    });
+    break;
+    
+    case 1:
+        $.ajax({
+        url: baseUrl + "/ehr",
+        type: 'POST',
+        success: function (data) {
+            var ehrId = data.ehrId;
+            var partyData = {
+                firstNames: "Luka",
+                lastNames: "Tavčer",
+                dateOfBirth: "1996-10-30T14:58",
+                partyAdditionalInfo: [{key: "ehrId", value: ehrId}]
+            };
+            $.ajax({
+                url: baseUrl + "/demographics/party",
+                type: 'POST',
+                contentType: 'application/json',
+                data: JSON.stringify(partyData),
+                success: function (party) {
+                    if (party.action == 'CREATE') {
+                        $("#kreirajSporocilo2").html("<span class='label label-primary'>EHR '" +
+                      ehrId + "'</span>");
+                    }
+                },
+                error: function(err) {
+                	$("#kreirajSporocilo").html("<span class='obvestilo label " +
+                "label-danger fade-in'>Napaka '" +
+                JSON.parse(err.responseText).userMessage + "'!");
+                }
+            });
+            
+        	var podatki = {
+        	    "ctx/language": "en",
+        	    "ctx/territory": "SI",
+        	    "ctx/time": "1938-10-30T14:58",
+        	    "vital_signs/height_length/any_event/body_height_length": "185",
+        	    "vital_signs/body_weight/any_event/body_weight": "55.0",
+        	   	"vital_signs/body_temperature/any_event/temperature|magnitude": "36.50",
+        	    "vital_signs/body_temperature/any_event/temperature|unit": "°C",
+        	    "vital_signs/blood_pressure/any_event/systolic": "118",
+        	    "vital_signs/blood_pressure/any_event/diastolic": "92",
+        	    "vital_signs/indirect_oximetry:0/spo2|numerator": "98"
+            };
+        	var parametriZahteve = {
+        	    ehrId: ehrId,
+        	    templateId: 'Vital Signs',
+        	    format: 'FLAT',
+        	    committer: 'Medicinska sestra Anja'
+        	};
+            
+            $.ajax({
+        	    url: baseUrl + "/composition?" + $.param(parametriZahteve),
+        	    type: 'POST',
+        	    contentType: 'application/json',
+        	    data: JSON.stringify(podatki)   
+            });
+        }
+    });
+    break;
+    
+    case 2:
+        $.ajax({
+        url: baseUrl + "/ehr",
+        type: 'POST',
+        success: function (data) {
+            var ehrId = data.ehrId;
+            var partyData = {
+                firstNames: "Luka",
+                lastNames: "Tavčer",
+                dateOfBirth: "1996-10-30T14:58",
+                partyAdditionalInfo: [{key: "ehrId", value: ehrId}]
+            };
+            $.ajax({
+                url: baseUrl + "/demographics/party",
+                type: 'POST',
+                contentType: 'application/json',
+                data: JSON.stringify(partyData),
+                success: function (party) {
+                    if (party.action == 'CREATE') {
+                        $("#kreirajSporocilo3").html("<span class='label label-primary'>EHR 3 '" +
+                      ehrId + "'</span>");
+                    }
+                },
+                error: function(err) {
+                	$("#kreirajSporocilo").html("<span class='obvestilo label " +
+                "label-danger fade-in'>Napaka '" +
+                JSON.parse(err.responseText).userMessage + "'!");
+                }
+            });
+            
+        	var podatki = {
+        	    "ctx/language": "en",
+        	    "ctx/territory": "SI",
+        	    "ctx/time": "1938-10-30T14:58",
+        	    "vital_signs/height_length/any_event/body_height_length": "185",
+        	    "vital_signs/body_weight/any_event/body_weight": "55.0",
+        	   	"vital_signs/body_temperature/any_event/temperature|magnitude": "36.50",
+        	    "vital_signs/body_temperature/any_event/temperature|unit": "°C",
+        	    "vital_signs/blood_pressure/any_event/systolic": "118",
+        	    "vital_signs/blood_pressure/any_event/diastolic": "92",
+        	    "vital_signs/indirect_oximetry:0/spo2|numerator": "98"
+            };
+        	var parametriZahteve = {
+        	    ehrId: ehrId,
+        	    templateId: 'Vital Signs',
+        	    format: 'FLAT',
+        	    committer: 'Medicinska sestra Anja'
+        	};
+            
+            $.ajax({
+        	    url: baseUrl + "/composition?" + $.param(parametriZahteve),
+        	    type: 'POST',
+        	    contentType: 'application/json',
+        	    data: JSON.stringify(podatki)   
+            });
+        }
+    });
+        
+    break;
+        
+    default:
+        return 0;
+}
 
   // TODO: Potrebno implementirati
 
@@ -40,3 +223,19 @@ function generirajPodatke(stPacienta) {
 
 
 // TODO: Tukaj implementirate funkcionalnost, ki jo podpira vaša aplikacija
+
+function kreirajEHRzaBolnika() {
+	sessionId = getSessionId();
+	
+	$.ajaxSetup({
+	    headers: {"Ehr-Session": sessionId}
+	});
+	
+	
+	
+	for(var i=0; i<3; i++) {
+	    generirajPodatke(i);
+	    
+    
+}
+}
